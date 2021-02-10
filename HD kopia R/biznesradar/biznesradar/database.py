91431d2c22bef9d1b1905d0109e219f66089a6df
RadoslawMieckowski <76852512+RadoslawMieckowski@users.noreply.github.com>
2021-02-10 22:50:24 +0100

    Add files via upload

---
+++ HD kopia R/biznesradar/biznesradar/database.py
@@ -0,0 +1,17 @@
+# import sqlite3
+#
+# conn=sqlite3.connect('biznesradar.db')
+# curr=conn.cursor()
+#
+# # curr.execute("""CREATE TABLE indicators(      jeżeli istnieje, to wygenruje nam wyjątek, stąd komentarz
+# #             date TEXT, nameOfCompany TEXT, price REAL,
+# #             isToBuy TEXT, priceToBV REAL, priceToSalesPerShare REAL,
+# #             priceToProfit REAL, priceToOperationalProfit REAL,
+# #             ROE REAL, ROA REAL
+# #             )""")
+# conn.commit()
+# conn.close()
+
+
+# po skompilowaniu ten plik już się nam nie przyda
+# nie przyda nam się w ogóle, jeżeli będziemy robić bazę danych w chmurze.=>można skasować plik biznesradar.db
